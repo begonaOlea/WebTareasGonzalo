10f557c6d674d6f09f722ddb39703e9cff2da7d8
grinones1 <74243307+grinones1@users.noreply.github.com>
2020-12-22 12:15:31 +0100

    Commit 2

--- src/main/java/com/tareas/model/Tarea.java
+++ src/main/java/com/tareas/model/Tarea.java
@@ -16,4 +16,56 @@ public class Tarea {
     private String  descripcion;
     private boolean realizado;
     private Usuario usuario;
+
+    public Tarea(Integer id, String titulo, String descripcion, Usuario usuario) {
+        this.id = id;
+        this.titulo = titulo;
+        this.descripcion = descripcion;
+        this.realizado = false;
+        this.usuario = usuario;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public boolean isRealizado() {
+        return realizado;
+    }
+
+    public void setRealizado(boolean realizado) {
+        this.realizado = realizado;
+    }
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
+    
+    
+    
+    
 }
